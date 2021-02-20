f8a4acd812258ac5eb588ef6a2a47b4f3473ddc3
dangwin <danghwin@gmail.com>
2021-02-19 22:47:45 -0600

    commit tree 3bec1c05ef8257ea1a5b9dbcdbc55e684ebb105d

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function
+ destructivelyAppendDriver(driver){
+     drivers.push(driver);
+ }
+
+ function destructivelyPrependDriver(driver){
+     drivers.unshift(driver);
+ }
+
+function destructivelyRemoveLastDriver(driver){
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(driver){
+    drivers.shift();
+} 
+
+function appendDriver(driver){
+    const driver1 = [...drivers, driver]
+    return driver1
+}
+
+function prependDriver(driver){
+    const driver2 = [driver, ...drivers]
+    return driver2
+}
+
+function removeLastDriver(){
+    const driver = drivers.slice(0, 2)
+    return driver
+}
+
+function removeFirstDriver(){
+    const driver = drivers.slice(1, 3)
+    return driver
+}
